a78d22eb1acc0030d1f14ea903e19418b96d1cdd
Zillur Rahman <45931366+rullizai@users.noreply.github.com>
2019-11-12 08:55:35 +0600

    Add files via upload

---
+++ DbConnect.py
@@ -0,0 +1,20 @@
+import os
+import psycopg2
+import cv2
+import numpy as np
+import re
+
+
+
+DATABASE_USER = os.environ['DATABASE_USER']
+DATABASE_PASSWORD = os.environ['DATABASE_PASSWORD']
+DATABASE_HOST = os.environ['DATABASE_HOST']
+DATABASE_PORT = os.environ['DATABASE_PORT']
+DATABASE_NAME = os.environ['DATABASE_NAME']
+
+def DATABASE_CONNECTION():
+    return psycopg2.connect(user=DATABASE_USER,
+                              password=DATABASE_PASSWORD,
+                              host=DATABASE_HOST,
+                              port=DATABASE_PORT,
+                              database=DATABASE_NAME)
---
+++ SendData.py
@@ -0,0 +1,6 @@
+import os
+import psycopg2
+import cv2
+import numpy as np
+import re
+import DbConnect
